17804e3c16974aac1aaf8368912a204299c102ff
amshelhack3r <amshelhack3r@gmail.com>
2018-10-10 01:59:32 +0300

    new success route

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 /* GET home page. */
-router.get('/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }));
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
-router.get('/cb',
-    passport.authenticate('google', {
-        failureRedirect: '/login'
-    }),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
+router.get(
+  "/cb",
+  passport.authenticate("google", {
+    failureRedirect: "/login"
+  }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/oauth/auth/success");
+  }
+);
 
-router.get('/facebook',
-    passport.authenticate('facebook'));
+router.get("/facebook", passport.authenticate("facebook"));
 
-router.get('/cb2',
-    passport.authenticate('facebook', {
-        failureRedirect: '/login'
-    }),
-    (req, res) => {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
+router.get(
+  "/cb2",
+  passport.authenticate("facebook", {
+    failureRedirect: "/oauth"
+  }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect("/oauth/auth/success");
+  }
+);
+
+router.get("/success", (req, res) => {});
 module.exports = router;
