d45b5ee266c01bc6c4685a489708b5111f68b739
amshelhack3r <amshelhack3r@gmail.com>
2018-10-10 00:18:46 +0300

    change redirect rout

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,13 @@
 module.exports = {
-    google_auth: {
-        clientID: '74623604375-08ib4tv6n1ojca3lh73e0m6pn2u0322n.apps.googleusercontent.com',
-        clientSecret: 'XU4ZMFblXS27UEuAdtvLBmj9',
-        callback: '/auth/cb'
-    },
-    facebook_auth: {
-        clientID: '587193768365731',
-        clientSecret: 'b28f2b8fb72886257fd89fd9303e3132',
-        callback: '/auth/cb2'
-    }
-
-}
+  google_auth: {
+    clientID:
+      "74623604375-08ib4tv6n1ojca3lh73e0m6pn2u0322n.apps.googleusercontent.com",
+    clientSecret: "XU4ZMFblXS27UEuAdtvLBmj9",
+    callback: "/oauth/auth/cb"
+  },
+  facebook_auth: {
+    clientID: "587193768365731",
+    clientSecret: "b28f2b8fb72886257fd89fd9303e3132",
+    callback: "/oauth/auth/cb2"
+  }
+};
