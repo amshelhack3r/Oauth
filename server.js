17e433f5f7dad96c4257f05d668ee6d7330fc32f
amshelhack3r <amshelhack3r@gmail.com>
2018-10-10 09:35:11 +0300

    save the sessions

--- server.js
+++ server.js
@@ -47,10 +47,7 @@ app.use(
   session({
     secret: "keyboard cat",
     resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: true
-    }
+    saveUninitialized: false
   })
 );
 
@@ -69,7 +66,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.locals.user = req.user || null;
 
   // render the error page
   res.status(err.status || 500);
